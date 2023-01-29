778368e227bfc7cf8cfb5f5b7f02f89f7870f1ec
Trent Townsend <trent.townsend2@gmail.com>
2023-01-29 16:54:00 +1000

    Add header titles and contact buttons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,6 +8,10 @@ export default function Header() {
                 <h1 id='name'>Laura Smith</h1>
                 <h2 id='role-title'>Frontend Developer</h2>
                 <span id='url'>laurasmith.website</span>
+                <div className='contact-btn'>
+                    <button id="email-btn"><span id="mail-icon"></span>Email</button>
+                    <button id="linkedin-btn"><span id="linkedin-icon"></span>LinkedIn</button>
+                </div>
             </div>
         </header>
     )
