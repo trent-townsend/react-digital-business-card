177a4511277dd469c8c6fa3156876bbf8ef0de29
Trent Townsend <trent.townsend2@gmail.com>
2023-01-29 22:58:32 +1000

    Config update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-digital-business-card/"
+  base: "/react-digital-business-card",
 }
 )
