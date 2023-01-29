4de9ce54438a76f487619832617826f91c423dbb
Trent Townsend <trent.townsend2@gmail.com>
2023-01-29 18:05:46 +1000

    Add social media icons in card footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,38 @@
+import "../components/footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+
+<a href="#">
+    <img
+    src="src/assets/icons/twitter.svg" 
+    alt ="twitter link"
+    className="social-icon"/>
+    </a>
+
+    <a href="#">
+    <img
+    src="src/assets/icons/square-facebook.svg" 
+    alt ="facebook link"
+    className="social-icon"/>
+    </a>
+
+    <a href="#">
+    <img
+    src="src/assets/icons/instagram.svg"
+    alt="instagram link"
+    className="social-icon" />
+    </a>
+
+    <a href="#">
+    <img
+    src="src/assets/icons/square-github.svg" 
+    alt ="github link"
+    className="social-icon"/>
+    </a>
+
+    </footer>
+
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from "./components/header"
 import MainText from './components/main-text'
+import Footer from './components/footer'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -11,6 +12,7 @@ function Page () {
   <div className="container">
     <Header />
     <MainText />
+    <Footer />
   </div>
   )
 }
